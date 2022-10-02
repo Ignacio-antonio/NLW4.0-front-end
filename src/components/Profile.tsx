341d4c831c71befd18246ccd80d0163fc7dcb021
Ignacio-antonio <yignaciosd@gmail.com>
2022-10-01 22:06:37 -0300

    initial commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,16 @@
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+    return(
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/ignacio-antonio.png" alt="Ignacio Antonio" />
+            <div>
+                <strong>Ignacio Antonio</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level" />
+                    Level 1
+                </p>
+            </div>
+        </div>
+    )
+}
